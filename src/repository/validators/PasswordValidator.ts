92fed769df5468cb40ef21c20dbf5c891a8849f1
cam <cjmooredev@gmail.com>
2025-05-07 22:18:19 -0400

    password validation function

---
+++ src/repository/validators/PasswordValidator.ts
@@ -0,0 +1,26 @@
+export interface PasswordTestResult {
+  message: string;
+  isValid: boolean;
+}
+
+export const isPasswordValid = (password: string) => {
+  const passwordTestResult: PasswordTestResult = {
+    message: "",
+    isValid: true,
+  };
+
+  if (password.length < 8) {
+    passwordTestResult.message = "Password must be atleast 8 characters long";
+    passwordTestResult.isValid = false;
+    return passwordTestResult;
+  }
+
+  const strongPasswordRegExp = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})");
+
+  if (!strongPasswordRegExp.test(password)) {
+    passwordTestResult.message = "Password must contatin atleast 1 special character, 1 capital letter and 1 number";
+    passwordTestResult.isValid = false;
+  }
+
+  return passwordTestResult;
+}
