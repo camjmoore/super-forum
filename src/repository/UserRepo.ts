0fae13d9dd25234dca33e272044529a53df342e9
cam <cjmooredev@gmail.com>
2025-05-07 22:17:06 -0400

    return type Promise<UserResult> delegates the resolve or reject outcomes as arguments to the constructor

--- src/repository/UserRepo.ts
+++ src/repository/UserRepo.ts
@@ -6,10 +6,32 @@ import { isPasswordValid } from "./validators/PasswordValidator"
 const saltRounds = 10;
 
 export class UserResult {
-  constructor(public message?: Array<string>, public user?: User) {}
+  constructor(public messages?: Array<string>, public user?: User) {}
 }
 
+export const register = async (
+  email: string,
+  userName: string,
+  password: string
+): Promise<UserResult> => {
+  const result = isPasswordValid(password);
+  if (!result.isValid) {
+    return {
+      messages: [
+        "passwords must have min length of 8, 1 upper case character, 1 number and 1 symbol",
+      ],
+    };
+  }
 
+  const trimmedEmail = email.trim().toLowerCase();
+  const emailError = isEmailValid(trimmedEmail);
+  if (emailError) {
+    return {
+      messages: [emailError],
+    };
+  }
+
+}
 
 
 
