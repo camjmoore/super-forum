bebc9330784f35b4f7ff6d2cebc95a2cd4522b6c
cam <cjmooredev@gmail.com>
2025-05-11 21:14:06 -0400

    query result constructors for array of entities or single entity

--- src/repository/QueryResult.ts
+++ src/repository/QueryResult.ts
@@ -1,3 +1,7 @@
-export class QueryResult<T> {
+export class QuerySingleResult<T> {
+  constructor(public messages?: Array<string>, public entity?: T) {}
+}
+
+export class QueryArrayResult<T> {
   constructor(public messages?: Array<string>, public entities?: Array<T>) {}
 }
