fd68cffdaf77b496b749519a71436f18ab207b1a
cam <cjmooredev@gmail.com>
2025-06-19 16:19:26 -0400

    wire up first resolver

--- server/src/apollo.ts
+++ server/src/apollo.ts
@@ -3,6 +3,7 @@ import { addResolversToSchema } from '@graphql-tools/schema';
 import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
 import { ApolloServer } from '@apollo/server';
 import { ApolloContext } from './types';
+import resolvers from './resolvers';
 
 const schema = loadSchemaSync('./schema.graphql', { 
   loaders: [new GraphQLFileLoader()] 
