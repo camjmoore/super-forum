a313ea8b3217bd42c48b412e152f36e8cb63f6ee
cam <cjmooredev@gmail.com>
2025-06-19 18:50:00 -0400

    update result resolver imports

--- server/src/resolvers/index.ts
+++ server/src/resolvers/index.ts
@@ -2,13 +2,22 @@ import { ApolloContext } from "../types";
 import { Resolvers } from "../resolvers-types.generated";
 import { Query } from "./queryResolvers";
 import { Mutation } from "./mutationResolvers";
-import { Results } from "./resultResolvers";
+import { 
+  UserResult,
+  ThreadResult,
+  ThreadArrayResult,
+  ThreadItemResult,
+  ThreadItemArrayResult,
+} from "./resultResolvers";
 
 const resolvers: Resolvers<ApolloContext> = {
    Query,
    Mutation,
-   Results,
-   // not sure if Results needs to be imported here or to Queries
+   UserResult,
+   ThreadResult,
+   ThreadArrayResult,
+   ThreadItemResult,
+   ThreadItemArrayResult,
 }
 
 export default resolvers;
