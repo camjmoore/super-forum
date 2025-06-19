92e9af9aa23c03f6b41730d5a52462f61c294ef7
cam <cjmooredev@gmail.com>
2025-06-19 17:57:01 -0400

    outlined itinerary of queries to implement

---
+++ server/src/resolvers/queryResolvers.ts
@@ -0,0 +1,12 @@
+import { QueryResolvers } from "../resolvers-types.generated";
+
+export const Queries: QueryResolvers = {
+   getThreadById: () => { throw new Error('Not Implemented')},
+   getThreadsByCategoryId: () => { throw new Error('Not Implemented')},
+   getThreadsLatest: () => { throw new Error('Not Implemented')},
+   getThreadItemByThreadId: () => { throw new Error('Not Implemented')},
+   getAllCategories: () => { throw new Error('Not Implemented')},
+   me: () => { throw new Error('Not Implemented')},
+   getTopCategoryThread: () => { throw new Error('Not Implemented')},
+
+}
