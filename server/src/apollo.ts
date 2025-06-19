ac3197c5f55792e00cf866ab6d96fa03dc58c5bf
cam <cjmooredev@gmail.com>
2025-06-18 23:21:30 -0400

    outlined apollo server but still need to define and pass resolvers

---
+++ server/src/apollo.ts
@@ -0,0 +1,17 @@
+import { loadSchemaSync } from '@graphql-tools/load';
+import { addResolversToSchema } from '@graphql-tools/schema';
+import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
+import { ApolloServer } from '@apollo/server';
+import { ApolloContext } from './types';
+
+const schema = loadSchemaSync('./schema.graphql', { 
+  loaders: [new GraphQLFileLoader()] 
+});
+
+const schemaWithResolvers = addResolversToSchema({ schema, resolvers });
+
+export async function createApolloServer() {
+  return new ApolloServer<ApolloContext>({
+    schema: schemaWithResolvers
+  });
+}
