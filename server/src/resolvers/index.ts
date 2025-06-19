3ebafcd83145983c989ac019728f8a388419c12c
cam <cjmooredev@gmail.com>
2025-06-19 18:23:50 -0400

    wire up mutation resolvers and resolve types to top level resolver

--- server/src/resolvers/index.ts
+++ server/src/resolvers/index.ts
@@ -1,9 +1,14 @@
 import { ApolloContext } from "../types";
 import { Resolvers } from "../resolvers-types.generated";
 import { Queries } from "./queryResolvers";
+import { Mutations } from "./mutationResolvers";
+import { Results } from "./resultResolvers";
 
- const resolvers: Resolvers<ApolloContext> = {
-    Queries
- }
+const resolvers: Resolvers<ApolloContext> = {
+   Queries,
+   Mutations,
+   Results,
+   // not sure if Results needs to be imported here or to Queries
+}
 
- export default resolvers;
+export default resolvers;
