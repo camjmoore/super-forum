16b90b76fd913da8931daf2c2c75e4d97a28f3f9
cam <cjmooredev@gmail.com>
2025-06-19 22:46:40 -0400

    remove result resolvers

--- server/src/resolvers/index.ts
+++ server/src/resolvers/index.ts
@@ -2,22 +2,10 @@ import { ApolloContext } from "../types";
 import { Resolvers } from "../resolvers-types.generated";
 import { Query } from "./queryResolvers";
 import { Mutation } from "./mutationResolvers";
-import { 
-  UserResult,
-  ThreadResult,
-  ThreadArrayResult,
-  ThreadItemResult,
-  ThreadItemArrayResult,
-} from "./resultResolvers";
 
 const resolvers: Resolvers<ApolloContext> = {
    Query,
    Mutation,
-   UserResult,
-   ThreadResult,
-   ThreadArrayResult,
-   ThreadItemResult,
-   ThreadItemArrayResult,
 }
 
 export default resolvers;
