9fc0c0606af9914a7887eb482025c3e37c36c677
cam <cjmooredev@gmail.com>
2025-06-21 23:50:37 -0400

    first query resolver works

--- server/src/resolvers/queryResolvers.ts
+++ server/src/resolvers/queryResolvers.ts
@@ -1,7 +1,23 @@
-import { QueryResolvers } from "../resolvers-types.generated";
+import { QueryResolvers, ThreadResult } from "../resolvers-types.generated";
+import { getThreadById } from "../repository/ThreadRepo";
+
 
 export const Query: QueryResolvers = {
-   getThreadById: () => { throw new Error('Not Implemented')},
+   getThreadById: async (_, { id }): Promise<ThreadResult> => {
+      const { entity, messages } = await getThreadById(id)
+
+      if (entity) {
+         return {
+            __typename: "Thread",
+            ...entity
+         }
+      }
+
+      return {
+         __typename: 'EntityResult',
+         messages
+      }
+   },
    getThreadsByCategoryId: () => { throw new Error('Not Implemented')},
    getThreadsLatest: () => { throw new Error('Not Implemented')},
    getThreadItemByThreadId: () => { throw new Error('Not Implemented')},
