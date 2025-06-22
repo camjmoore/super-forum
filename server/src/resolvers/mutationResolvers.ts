bb7feccdf08593c8287d918485e92043db2def66
cam <cjmooredev@gmail.com>
2025-06-21 21:10:15 -0400

    simplified resolver and remove try catch

--- server/src/resolvers/mutationResolvers.ts
+++ server/src/resolvers/mutationResolvers.ts
@@ -1,6 +1,5 @@
-import { ApolloContext } from "../types";
 import { Thread, EntityResult, MutationResolvers } from "../resolvers-types.generated";
-import { register, login, logout }  from "./repository/UserRepo";
+import { register, login, logout }  from "../repository/UserRepo";
 import { createThread, getThreadsByCategoryId }  from "../repository/ThreadRepo";
 import { QuerySingleResult, QueryArrayResult } from "../repository/QueryResult";
 import { 
@@ -12,25 +11,14 @@ import {
  } from "./resultResolvers";
 
 export const Mutation: MutationResolvers = {
-   // ts convention to underscore the unused args positioned in front of the one you are consuming
-   // graphql convention to omit remaining args if unused
-   createThread: async (_, args: { userId: string; categoryId: string; title: string; body: string; }
-   ): Promise<EntityResult> => {
-      let result: QuerySingleResult<Thread>;
-
-      try {
-         result = await createThread(args.userId, args.categoryId, args.title, args.body);
-
-         return {
-            __typename: 'EntityResult',
-            messages: result.messages
-               ? result.messages
-               : ["An error has occurred"],
-         };
-      } catch (ex) {
-         //maybe expand on this with some additional logging rather than rethrowing the 
-         throw ex;
-      }
+   // ts convention to underscore unused args
+   // graphql convention to omit remaining unused args
+   createThread: async (_, { userId, categoryId, title, body }): Promise<EntityResult> => {
+      const { messages } = await createThread(userId, categoryId, title, body);
+      return {
+         __typename: 'EntityResult',
+         messages: messages
+      };
    },
    createThreadItem: () => { throw new Error('Not Implemented')},
    register: () => { throw new Error('Not Implemented')},
