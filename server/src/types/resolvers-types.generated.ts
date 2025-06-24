a3e89752b6458e2311bda44120425434572ceb1a
vespertine <cjmooredev@gmail.com>
2025-06-24 17:54:06 -0400

    generated types now output to types directory

--- server/src/types/resolvers-types.generated.ts
+++ server/src/types/resolvers-types.generated.ts
@@ -1,5 +1,5 @@
 import { GraphQLResolveInfo, GraphQLScalarType, GraphQLScalarTypeConfig } from 'graphql';
-import { ApolloContext } from './types';
+import { ApolloContext } from './IApolloContext';
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
