e8431549bed60b7c469d43c4c7f680e9b1b1d18f
cam <cjmooredev@gmail.com>
2025-10-27 15:28:20 -0400

    imported domain-based resolvers and unions to compose top level resolver

--- server/src/resolvers/index.ts
+++ server/src/resolvers/index.ts
@@ -1,11 +1,75 @@
 import { ApolloContext } from '../types/IApolloContext';
 import { Resolvers } from '../types/resolvers-types.generated';
-import { Query } from './queryResolvers';
-import { Mutation } from './mutationResolvers';
 
+// Import domain-specific resolvers
+import {
+  userQueries,
+  userMutations,
+  userFieldResolvers,
+} from './user/user.resolvers';
+import {
+  threadQueries,
+  threadMutations,
+  threadFieldResolvers,
+} from './thread/thread.resolvers';
+import {
+  threadItemQueries,
+  threadItemMutations,
+  threadItemFieldResolvers,
+} from './threadItem/threadItem.resolvers';
+import {
+  categoryQueries,
+  categoryMutations,
+  threadCategoryFieldResolvers,
+} from './category/category.resolvers';
+import {
+  pointsMutations,
+  threadPointFieldResolvers,
+  threadItemPointFieldResolvers,
+} from './points/points.resolvers';
+
+// Import union type resolvers
+import {
+  UserResult,
+  ThreadResult,
+  ThreadArrayResult,
+  ThreadItemResult,
+  ThreadItemArrayResult,
+} from './unions';
+
+// Aggregate all resolvers by type
 const resolvers: Resolvers<ApolloContext> = {
-  Query,
-  Mutation,
+  // Top-level Query resolvers from all domains
+  Query: {
+    ...userQueries,
+    ...threadQueries,
+    ...threadItemQueries,
+    ...categoryQueries,
+  },
+
+  // Top-level Mutation resolvers from all domains
+  Mutation: {
+    ...userMutations,
+    ...threadMutations,
+    ...threadItemMutations,
+    ...categoryMutations,
+    ...pointsMutations,
+  },
+
+  // Field resolvers for complex types
+  User: userFieldResolvers,
+  Thread: threadFieldResolvers,
+  ThreadItem: threadItemFieldResolvers,
+  ThreadCategory: threadCategoryFieldResolvers,
+  ThreadPoint: threadPointFieldResolvers,
+  ThreadItemPoint: threadItemPointFieldResolvers,
+
+  // Union type resolvers
+  UserResult,
+  ThreadResult,
+  ThreadArrayResult,
+  ThreadItemResult,
+  ThreadItemArrayResult,
 };
 
 export default resolvers;
