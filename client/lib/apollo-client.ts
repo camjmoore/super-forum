bb5977a0519b3e560fec3f1bda5bc0beffdbe4c4
Cameron Moore <cjmooredev@gmail.com>
2026-06-11 23:48:07 -0400

    Fix Apollo Client URI construction

--- client/lib/apollo-client.ts
+++ client/lib/apollo-client.ts
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 export function makeApolloClient() {
   return new ApolloClient({
     link: new HttpLink({
-      uri: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000/graphql',
+      uri: `${process.env.NEXT_PUBLIC_API_URL}/graphql` || 'http://localhost:4000/graphql',
       credentials: 'include',
     }),
     cache: new InMemoryCache(),
